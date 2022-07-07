6c01ba77b2a56f59a7be246bdfaa443603a382e5
xryuseix <ryusei143.shootingstar@gmail.com>
2022-07-07 17:58:43 +0900

    feat: add zip module

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -6,7 +6,7 @@
  * @returns ex.[[a,b,c], [d,e,f]]
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const zip = <T>(...arrays : T[][]): T[][] => {
-  const length = Math.min(...arrays.map((arr:T[]) => arr.length));
+const zip = <T>(...arrays: T[][]): T[][] => {
+  const length = Math.min(...arrays.map((arr: T[]) => arr.length));
   return new Array(length).fill(0).map((_, i) => arrays.map((arr) => arr[i]));
 };

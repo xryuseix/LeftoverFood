77e3f7e533b038b3463ad1e33a03389c9057c972
xryuseix <ryusei143.shootingstar@gmail.com>
2022-07-07 17:58:17 +0900

    feat: MetaResponseのレスポンスフォーマットを変更

--- api/src/dataset.ts
+++ api/src/dataset.ts
@@ -9,7 +9,7 @@ type APIResponse = {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const doGet = (_event: GoogleAppsScript.Events.DoGet) => {
   const sheets = SpreadsheetApp.getActiveSpreadsheet();
-  const content = { data: getMetaData(sheets) };
+  const content = getMetaData(sheets);
   return toResponseFormat("success", content);
 };
 
@@ -34,8 +34,9 @@ const toResponseFormat = (
 /**
  * マスクされた電話番号に対するメタデータを取得する
  * @param sheets スクリプトに紐づけられたスプレッドシート
- * @returns メタデータ
+ * @returns MetaResponse Schema
  */
+// TODO: 型定義を追加する
 const getMetaData = (sheets: GoogleAppsScript.Spreadsheet.Spreadsheet) => {
   const sheet = sheets.getSheetByName("UNIQUE");
   const lastColChar = String.fromCharCode(
@@ -44,5 +45,41 @@ const getMetaData = (sheets: GoogleAppsScript.Spreadsheet.Spreadsheet) => {
   const metaData: string[][] = sheet
     .getRange(`A1:${lastColChar}${sheet.getLastRow()}`)
     .getValues();
-  return metaData;
+
+  const columnTitles = zip(metaData[0].slice(2), metaData[1].slice(2)).map(
+    (col: string[]) => {
+      return {
+        phoneOrEmail: col[0],
+        columnTitle: col[1],
+      };
+    }
+  );
+  const phoneColumnTitles = columnTitles.filter(
+    (columnTitle) => columnTitle.phoneOrEmail === "PHONE"
+  );
+  const emailColumnTitles = columnTitles.filter(
+    (columnTitle) => columnTitle.phoneOrEmail === "MAIL"
+  );
+  const metaResponse = metaData.slice(2).map((row: string[]) => {
+    const leakedInfo = row[0];
+    const phoneOrEmail = row[1] === "PHONE" ? "phone" : "email";
+    const getContents = (): string[][] => {
+      if (phoneOrEmail === "phone") {
+        const phoneContents = zip(
+          phoneColumnTitles.map((col) => col.columnTitle),
+          row.slice(2)
+        );
+        return phoneContents;
+      } else {
+        const emailContents = zip(
+          emailColumnTitles.map((col) => col.columnTitle),
+          row.slice(2)
+        );
+        return emailContents;
+      }
+    };
+    return [leakedInfo, Object.fromEntries(getContents())];
+  });
+  console.log(Object.fromEntries(metaResponse));
+  return Object.fromEntries(metaResponse);
 };
---
+++ api/src/utils.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+/**
+ * Pythonのzipのように2つのリストを結合する
+ * @param arrays ex.([a,d] [b,e] [c,f])
+ * @returns ex.[[a,b,c], [d,e,f]]
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const zip = <T>(...arrays : T[][]): T[][] => {
+  const length = Math.min(...arrays.map((arr:T[]) => arr.length));
+  return new Array(length).fill(0).map((_, i) => arrays.map((arr) => arr[i]));
+};

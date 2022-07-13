79173b8736dcc2e787323b9918853306e3b7b1f1
xryuseix <ryusei143.shootingstar@gmail.com>
2022-07-14 01:45:23 +0900

    test: add zip util test

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -2,11 +2,12 @@
 
 /**
  * Pythonのzipのように2つのリストを結合する
- * @param arrays ex.([a,d] [b,e] [c,f])
- * @returns ex.[[a,b,c], [d,e,f]]
+ * @param arrays ex.[1, 2, 3], [4, 5, 6]
+ * @returns ex.[1, 4],[2, 5],[3, 6]
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const zip = <T>(...arrays: T[][]): T[][] => {
+export const zip = <T>(...arrays: T[][]): T[][] => {
   const length = Math.min(...arrays.map((arr: T[]) => arr.length));
   return new Array(length).fill(0).map((_, i) => arrays.map((arr) => arr[i]));
 };
+
+export const add = (a: number, b: number): number => a + b;
